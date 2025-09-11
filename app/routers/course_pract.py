68cd9685b238d8e0d45e39491dbcbe9d51d88427
lokhandeganesh <lokhandeganesh8@gmail.com>
2025-09-11 14:32:18 +0530

    implemented Delete mechanism

--- app/routers/course_pract.py
+++ app/routers/course_pract.py
@@ -1,6 +1,6 @@
 from typing import Optional
-from fastapi import APIRouter
-from fastapi.params import Body
+from fastapi import APIRouter, Response, status, HTTPException
+# from fastapi.params import Body
 from pydantic import BaseModel
 
 from random import randrange
@@ -62,16 +62,61 @@ my_posts = [
 	}
 ]
 
+def find_post(id):
+	for p in my_posts:
+		if (p["id"] == id):
+			return p
+
+def find_index_post(id):
+	for i, p in enumerate(my_posts):
+		if (p["id"] == id) :
+			return i
+
 @router.get("/posts")
 async def get_posts():
 	return {"data" : my_posts}
 
 # post method to create post
-@router.post("/posts")
-async def create_posts(post: Post):
+@router.post("/posts", status_code = status.HTTP_201_CREATED)
+async def create_posts(post:Post):
 	post_dict = post.model_dump()
 	post_dict['id'] = randrange(0, 1000000000)
 	my_posts.append(post_dict)
 
 	# print(post_dict)
 	return {"data" : my_posts}
+
+# to retrive latest post from posts
+@router.get("/posts/latest")
+async def get_latest_post():
+	post = my_posts[len(my_posts) - 1]
+	return {"data" : post}
+
+# to retrive post from posts
+@router.get("/posts/{id}")
+async def get_post(id:int, response: Response):
+	post  = find_post(id)
+
+	if not post:
+		# response.status_code = status.HTTP_404_NOT_FOUND
+		# return {"message" : f"post with id:{id} not found."}
+
+		raise HTTPException(
+			status_code = status.HTTP_404_NOT_FOUND,
+			detail = f"post with id:{id} not found."
+			)
+	return {"data" : post}
+
+@router.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
+async def delete_post(id:int):
+	# using index of in posts
+	index = find_index_post(id)
+
+	if not index:
+		raise HTTPException(
+			status_code= status.HTTP_404_NOT_FOUND,
+			detail = f"post with id:{id} not found."
+		)
+
+	my_posts.pop(index)
+	return Response(status_code=status.HTTP_204_NO_CONTENT)
