854967e17d2617578e66eda1345374c10a3b25c1
lokhandeganesh <lokhandeganesh8@gmail.com>
2025-12-22 01:03:21 +0530

    sqlalchemy CRUD operation completed

--- app/routers/course_pract.py
+++ app/routers/course_pract.py
@@ -57,7 +57,6 @@ class Post(BaseModel):
 	title: str
 	content: str
 	published: bool = True
-	rating : Optional[int] = 1
 	owner_id : int
 
 # @router.post("/create_posts_model")
@@ -74,14 +73,12 @@ my_posts = [
 	{
 		"id": 1 , "title" : "title of post 1",
 		"content" : "content of post 1",
-		"published": True,
-		"rating": 4
+		"published": True
 	},
 	{
 		"id": 2 , "title" : "title of post 2",
 		"content" : "content of post 2",
-		"published": True,
-		"rating": 4
+		"published": True
 	}
 ]
 
@@ -293,3 +290,72 @@ async def create_sqla_posts(post:Post, db:Session = Depends(get_db)):
 	db.refresh(new_post)
 
 	return {"data" : new_post}
+
+# to retrive post from posts
+@router.get("/sqla_posts/{id}")
+async def get_sqla_post(id:int, response: Response, db:Session = Depends(get_db)):
+
+	# query to get post by id
+	post = db.query(models.Post).filter(models.Post.id == id).first()
+
+	try:
+		if not post:
+			logger.info(f"post with id:{id} not found.")
+
+			raise HTTPException(
+				status_code = status.HTTP_404_NOT_FOUND,
+				detail = f"post with id:{id} not found."
+				)
+		return {"data" : post}
+	except HTTPException:
+		# Let FastAPI handle HTTPException (like 404)
+		raise
+	except Exception as e:
+		# Log the exception for debugging
+		logger.exception(f"Unhandled error: {e}")
+
+# to delete post from course.posts table
+@router.delete("/sqla_posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
+async def delete_sqla_post(id:int, db:Session = Depends(get_db)):
+
+	# query to delete post by id
+	post = db.query(models.Post).filter(models.Post.id == id)
+
+	if not post.first():
+		raise HTTPException(
+			status_code= status.HTTP_404_NOT_FOUND,
+			detail = f"post with id:{id} not found."
+		)
+
+	# delete the post
+	post.delete(synchronize_session=False)
+
+	# commit the changes to database
+	db.commit()
+
+	return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+# to update post from course.posts table
+@router.put("/sqla_posts/{id}")
+async def update_sqla_post(id:int, post:Post, db:Session = Depends(get_db)):
+
+	# query to update post by id
+	post_query = db.query(models.Post).filter(models.Post.id == id)
+
+	# retrive the post for updating
+	updated_post = post_query.first()
+
+	# using index of in posts
+	if not updated_post:
+		raise HTTPException(
+			status_code= status.HTTP_404_NOT_FOUND,
+			detail = f"post with id:{id} not found."
+		)
+
+	# update the post
+	post_query.update(post.model_dump(), synchronize_session = False)
+
+	# commit the changes to database
+	db.commit()
+
+	return {"data" : post_query.first()}
