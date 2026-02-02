a0c34f40f35531d99ff9c5198b656ea7e4ca9b5f
lokhandeganesh <lokhandeganesh8@gmail.com>
2026-02-03 00:55:28 +0530

    commented unused code

--- app/routers/course_pract.py
+++ app/routers/course_pract.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import List
 from fastapi import APIRouter, Response, status, HTTPException
 # from fastapi.params import Body
 from pydantic import BaseModel
