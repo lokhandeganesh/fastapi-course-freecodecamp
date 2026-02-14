82c3bc0410e787c3d207f98e512328c2e3ec6005
lokhandeganesh <lokhandeganesh8@gmail.com>
2026-02-14 15:55:12 +0530

    test_create_user added to conftest.py to accessesible to all future test cases

--- using_pytest/conftest.py
+++ using_pytest/conftest.py
@@ -1,4 +1,5 @@
 from .databasetest import session, client
+import pytest
 """
     Share Fixtures -
     Any fixtures defined in conftest.py are available
@@ -6,3 +7,25 @@ from .databasetest import session, client
     files in that directory and any subdirectories.
 """
 __all__ = ["session", "client"]
+
+
+@pytest.fixture
+def test_create_user(client):
+    user_data = {
+            "email": "test@example.com",
+            "password": "testpassword"
+            }
+
+    response = client.post(
+        url = "/course_users/",
+        json = user_data)
+
+    # new_user = schemas.UserOut(**response.json())
+    new_user = response.json()
+    print(new_user)
+
+    new_user["password"] = user_data["password"]
+
+    assert new_user["email"] == "test@example.com"
+    assert response.status_code == 201
+    return new_user
