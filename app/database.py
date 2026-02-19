35f289cfa30cf8c0c00c48d9f2b5490332fdb7e5
lokhandeganesh <lokhandeganesh8@gmail.com>
2026-02-20 03:45:12 +0530

    deploying docker for CD

--- app/database.py
+++ app/database.py
@@ -13,7 +13,7 @@ from .config import settings
 
 # or we can import database_url from settings
 SQLALCHEMY_DATABASE_URL = settings.database_url
-print(SQLALCHEMY_DATABASE_URL)
+# print(SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
